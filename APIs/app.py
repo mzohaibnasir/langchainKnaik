fa20a6bd0a0cd3865cf65090aa336ff4747d815a
M-Zohaib Nasir <m.zohaibnasir6@gmail.com>
2024-04-26 22:28:06 +0500

    up

--- APIs/app.py
+++ APIs/app.py
@@ -1,1 +1,51 @@
 from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+
+
+from langchain_community.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn
+import os
+
+
+from langchain_community.llms import ollama
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.environ("OPENAI_API_KEY")
+
+
+APP = FastAPI(
+    title="Langchain Server", version="1.0", description="A simple API server"
+)
+
+# adding routes
+add_routes(app, ChatOpenAI(), path="/openai")
+
+# to integrate prompt template with the route
+# model1
+llmOpenAI = ChatOpenAI()
+# model2
+llmLlama = ollama(model="llama2")
+
+
+# will interact with openAI
+prompt1 = ChatPromptTemplate.from_template(
+    "Write me an essay about {topic} with 100 words"
+)
+
+# will interact with llama2
+prompt2 = ChatPromptTemplate.from_template(
+    "Write me a poem about {topic} with 100 words"
+)
+
+
+add_routes(app, prompt1|llmOpenAI. path="/essay")
+
+add_routes(app, prompt2|llmLlama. path="/poem")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost",
+                port=8000)
